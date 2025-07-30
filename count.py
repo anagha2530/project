456c6aac98d9f170a90b7f90c63de61f923c3e64
anagha <anaghaakilesh@gmail.com>
2025-07-30 09:04:52 +0000

    Updated book return logic

--- count.py
+++ count.py
@@ -1,13 +1,13 @@
 from time import sleep
 
-def countdown(n):
+def countdon(n):
     print(n)
     sleep(0.5)
 
     if n == 1:
         return 
     else:
-        countdown(n-1)
+        countdon(n-1)
     
 n = int(input("enter the number : "))
-countdown(n)
+countdon(n)
