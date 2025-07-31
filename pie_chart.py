d2ad46d1dbc9404cce3d4df686791de43d4dfaf5
anagha <anaghaakilesh@gmail.com>
2025-07-31 12:04:06 +0000

    piechart added

---
+++ pie_chart.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+def main():
+    expenses = {}
+    while True:
+        expense_name = input("enter the name of the expense: ")
+        if expense_name == 'done':
+            break
+        expense_amount = input("enter the expense amount: ")
+        expenses[expense_name] = expense_amount
+    plot_expenses(expenses)
+
+def plot_expenses(expenses):
+    labels = list(expenses.keys())
+    values = list(expenses.values())
+
+    plt.figure(figsize=(10,7))
+    plt.pie(values, labels = labels, autopct="%1.1f%%",startangle=140)
+    plt.axis("equal")
+    plt.title("pie chart")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
